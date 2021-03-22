8bd0de78114a5504a2bff462dc741460a9cab2d4
ysonbrian <znlsakxls@gmail.com>
2021-03-22 15:38:52 +0900

    basic redux done

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import { createStore } from 'redux';
+
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
+
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
+
+// let count = 0;
+
+// number.innerText = count;
+
+// const updateText = () => {
+//   number.innerText = count;
+// };
+
+// const handleAdd = () => {
+//   count += 1;
+//   updateText();
+// };
+
+// const handleMinus = () => {
+//   count -= 1;
+//   updateText();
+// };
+// add.addEventListener('click', handleAdd);
+// minus.addEventListener('click', handleMinus);
